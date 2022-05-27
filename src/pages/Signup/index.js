07d84fa072c033f57158e86a798a8d8a50d104e3
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-05-28 03:20:10 +0900

    회원가입 마무리
    추가 수정 할 것 (나중에)
    1. 커서 위치
    2. 초기화 버튼?
    3. 보안성 문제

--- src/pages/Signup/index.js
+++ src/pages/Signup/index.js
@@ -1,4 +1,5 @@
 import { React, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import './Signup.scss';
 import { firestore } from '../../firebase';
 
@@ -11,6 +12,7 @@ const Signup = () => {
         Array.from({length:30}, (v,i) => i+1),
         Array.from({length:28}, (v,i) => i+1)        
     ];
+    const [errmsg, setErrmsg] = useState(false);
     const [inputs, setInputs] = useState({
         id: '',
         password: '',
@@ -20,17 +22,60 @@ const Signup = () => {
         month: '',
         day: '',
         sex: ''
-    })
+    });
+    const [isOk, setIsOk] = useState({
+        id: true,
+        password: true,
+        re_password: true,
+        id_check: false,
+    });
+
     const { id, password, re_password, name, year, month, day } = inputs;
+
+    let navigate= useNavigate();
 
     const CreateUser = (e) => {
         e.preventDefault();
-        const info = {id: '', password: '', name: '', year: '', month: '', day: '', sex: ''};
-        for (let key of Object.keys(info)) {
-            info[key] = e.target[key].value;
+        const info = {
+            id: '', 
+            password: '', 
+            name: '', 
+            year: '', 
+            month: '', 
+            day: '', 
+            sex: ''
+        };
+
+        if ( isOk.id_check && isOk.id ) {
+            for (let key of Object.keys(info)) {
+                info[key] = e.target[key].value;
+            }
+    
+            user_info.doc(info.id).set( { ...info, type: 'null' });
+            navigate('/login');
+        } else {
+            setErrmsg(true);
         }
-        user_info.doc(info.name).set(info);
-    }
+    };
+
+    const isAvailable_id = () => {
+        user_info.doc(inputs.id).get().then(doc => {
+            if (doc.exists) {
+                setIsOk({
+                    ...isOk,
+                    id: false,
+                    id_check: false
+                });
+            } else {
+                setIsOk({
+                    ...isOk,
+                    id: true,
+                    id_check: true
+                });
+            };
+            setErrmsg(false);
+        });
+    };
 
     const onChange = (e) => {
         const { name, value } = e.target;
@@ -50,28 +95,109 @@ const Signup = () => {
                 <form  onSubmit={CreateUser}>
                     <div className="signup-row">
                         <div className="signup-type">아이디</div>
-                        <div className="signup-input-area">
-                            <input name="id" value={id} onChange={onChange} className="signup-input" type="text" maxLength="20" placeholder="ID"/>
-                            <span className="util">유틸</span>
+                        <div className={ 
+                            errmsg ? 'signup-input-err' : 
+                            (isOk.id ? "signup-input-area" : 'signup-input-err')
+                        }>
+                            <input 
+                                name="id" 
+                                value={id} 
+                                onChange={onChange} 
+                                onFocus={(e) => e.target.placeholder=''}
+                                onBlur={(e) => e.target.placeholder='ID'}
+                                placeholder='ID'
+                                className="signup-input" 
+                                type="text" 
+                                maxLength="20"
+                            />
+                            <span 
+                                className="signup-id-check pointer"
+                                onClick={isAvailable_id}
+                            >
+                                중복확인
+                            </span>
                         </div>
+                        <p className={ isOk.id ? 'hide err-msg' : 'err-msg'}>
+                            다른 아이디를 이용하세요!
+                        </p>
+                        <p className={ errmsg ? 'err-msg' : 'hide err-msg'}>
+                            중복확인을 해주세요!
+                        </p>
                     </div>
                     <div className="signup-row">
                         <div className="signup-type">비밀번호</div>
-                        <div className="signup-input-area">
-                            <input name="password" value={password} onChange={onChange} className="signup-input" type="password" placeholder="비밀번호 (8~32자리)"/>
-                            <span className="util">유틸</span>
+                        <div className={
+                            isOk.password ? "signup-input-area" : 'signup-input-err'
+                        }>
+                            <input 
+                                name="password" 
+                                value={password} 
+                                onChange={onChange} 
+                                onFocus={(e) => e.target.placeholder=''}
+                                onBlur={(e) => {
+                                    if (e.target.value.length < 8 || e.target.value.length > 32) {
+                                        setIsOk({
+                                            ...isOk,
+                                            [e.target.name]: false
+                                        });
+                                    } else {
+                                        setIsOk({
+                                            ...isOk,
+                                            [e.target.name]: true
+                                        });
+                                    };
+                                }}
+                                placeholder="비밀번호 (8~32자리)"
+                                className="signup-input" 
+                                type="password" />
                         </div>
+                        <p className={ isOk.password ? 'hide err-msg' : 'err-msg'}>
+                            8~32자리의 비밀번호를 입력해주세요!                                
+                        </p>
+                    </div>
+                    <div className="signup-row">
                         <div className="signup-type">비밀번호 재확인</div>
-                        <div className="signup-input-area">
-                            <input name="re_password" value={re_password} onChange={onChange} className="signup-input" type="password" placeholder="비밀번호 재확인"/>
-                            <span className="util">유틸</span>
+                        <div className={
+                            isOk.re_password ? "signup-input-area" : 'signup-input-err'
+                        }>
+                            <input 
+                                name="re_password" 
+                                value={re_password} 
+                                onChange={onChange} 
+                                onFocus={(e) => e.target.placeholder=''}
+                                onBlur={(e) => {
+                                    e.target.placeholder="비밀번호 재확인";
+                                    if ( inputs.password !== e.target.value ) {
+                                        setIsOk({
+                                            ...isOk,
+                                            [e.target.name]: false
+                                        });
+                                    } else {
+                                        setIsOk({
+                                            ...isOk,
+                                            [e.target.name]: true
+                                        });
+                                    };
+                                }}
+                                placeholder="비밀번호 재확인"
+                                className="signup-input" 
+                                type="password" />
                         </div>
+                        <p className={ isOk.re_password ? 'hide err-msg' : 'err-msg'}>비밀번호가 일치하지 않습니다!</p>
                     </div>
                     <div className="signup-row">
                         <div className="signup-type">이름</div>
                         <div className="signup-input-area">
-                            <input name="name" value={name} onChange={onChange} className="signup-input" type="text" placeholder="이름"/>
-                            <span className="util">유틸</span>
+                            <input 
+                                name="name" 
+                                value={name} 
+                                onChange={onChange} 
+                                onFocus={(e) => e.target.placeholder=''}
+                                onBlur={(e)=> e.target.placeholder="이름"}
+                                placeholder="이름"
+                                className="signup-input" 
+                                type="text"
+                            />
                         </div>
                     </div>
                     <div className="signup-row">
@@ -101,17 +227,30 @@ const Signup = () => {
                         <div className="signup-type">성별</div>
                         <div className="signup-radio-area">
                             <div className="signup-radio-box">
-                                <input className="signup-radio" type="radio" name='sex' value='male' onChange={onChange}/>
+                                <input 
+                                    className="signup-radio" 
+                                    type="radio"    
+                                    name='sex' 
+                                    value='male' 
+                                    onChange={onChange}/>
                                 남성
                             </div>
                             <div className="signup-radio-box">
-                                <input className="signup-radio" type="radio" name='sex' value='female' onChange={onChange}/>
+                                <input 
+                                    className="signup-radio" 
+                                    type="radio" 
+                                    name='sex' 
+                                    value='female' 
+                                    onChange={onChange}/>
                                 여성
                             </div>
                         </div>
                     </div>
-                    <div className="signup-btn">
-                        <button className="pointer" type="submit">회원가입하기</button>
+                    <div className="signup-btn-area">
+                        <button 
+                            className={ (isOk.id && isOk.password && isOk.re_password ) ? "signup-btn pointer" : 'signup-btn' }
+                            disabled= { (isOk.id && isOk.password && isOk.re_password ) ? false : true }
+                            type="submit">회원가입</button>
                     </div>
                 </form>
             </main>

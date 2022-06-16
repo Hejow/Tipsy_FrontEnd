e05c44ba30e1047bd9a308a3309172dbc0fefeb8
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-06-16 13:28:46 +0900

    에러 페이지 구현

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -1,10 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import './Error.scss'
 
 const Error = () => {
+    const navigate = useNavigate();
     return (
-        <div className="Error-area">
-            Not Allowed to access;
+        <div className="error-area">
+            <div className="error-container">
+                <h2>4<span>0</span>4</h2>
+                <p className="error-ment"><span>Oops! </span> 잘못된 경로로 접속하셨어요!</p>
+                <div className="error-btn-area">
+                    <button className="error-btn pointer" onClick={() => {navigate('/')}}>홈으로 이동</button>
+                </div>
+            </div>
         </div>
     )
 };

ea854f0b7420abb336e8129d9c88ce5ca2ca7b1b
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-06-17 18:54:20 +0900

    에러 페이지 수정완료

--- src/pages/Error/index.js
+++ src/pages/Error/index.js
@@ -8,7 +8,10 @@ const Error = () => {
         <div className="error-area">
             <div className="error-container">
                 <h2>4<span>0</span>4</h2>
-                <p className="error-ment"><span>Oops! </span> 잘못된 경로로 접속하셨어요!</p>
+                <div className="error-ment-area">
+                    <span className="error-oops">Oops!</span>
+                    <span className="error-ment">잘못된 경로로 접속하셨어요!</span>
+                </div>
                 <div className="error-btn-area">
                     <button className="error-btn pointer" onClick={() => {navigate('/')}}>홈으로 이동</button>
                 </div>

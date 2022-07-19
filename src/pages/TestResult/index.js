e475834cc66f17c353014267a5f2fd1c7bd4ac8f
He Jow Moon <81742959+Hejow@users.noreply.github.com>
2022-07-19 12:00:47 +0900

    테스트결과 페이지 수정
    1. 레이아웃 수정

--- src/pages/TestResult/index.js
+++ src/pages/TestResult/index.js
@@ -14,44 +14,66 @@ const TestResult = () => {
             img:'/',
             name: '소주',
             color: '#51cf66',
-            ment: '멘트입니당ㅋ'
+            ment: '멘트입니당ㅋ',
+            comments: ['이 술에 대한 설명1', '이 술에 대한 설명2']
         },
         beer: {
             img:'/',
             name: '맥주',
             color: '#fcc419',
-            ment: '멘트입니당ㅋ'
+            ment: '멘트입니당ㅋ',
+            comments: ['이 술에 대한 설명1', '이 술에 대한 설명2']
         },
         wine: {
             img:'/',
             name: '와인',
             color: '#b21848',
-            ment: '멘트입니당ㅋ'
+            ment: '멘트입니당ㅋ',
+            comments: ['이 술에 대한 설명1', '이 술에 대한 설명2']
         },
         wiskey: {
             img:'/',
             name: '위스키',
             color: '#495057',
-            ment: '멘트입니당ㅋ'
+            ment: '멘트입니당ㅋ',
+            comments: ['이 술에 대한 설명1', '이 술에 대한 설명2']
         },
         cocktail: {
             img:'/',
             name: '칵테일',
             color: '#66d9e8',
-            ment: '골라먹는 재미가 있는'
+            ment: '어울리는 멘트',
+            comments: ['이 술에 대한 설명1', '이 술에 대한 설명2']
         }
     }
 
     return (
         <div className="testresult-area">
             <div className="testresult-contents">
-                <div className="testresult-ment">{Alcohols[test].ment}</div>
+                <h2 className="testresult-main-ment">나의 알코올 취향은?</h2>
                 <div className="testresult-mytype"
                     style={{color:Alcohols[test].color}}    >
                     {Alcohols[test].name}
                 </div>
                 <div className="testresult-img-area">
-                    <div className="testresult-img"></div>
+                    <div className="testresult-img">어울리는 사진</div>
+                </div>
+                <div className="testresult-ment">{Alcohols[test].ment}</div>
+                <div className="testresult-comment-area">
+                    {Alcohols[test].comments.map(comment => (
+                        <p className="testresult-comment" key={comment.toString()}>{comment}</p>
+                    ))}
+                </div>
+                <div className="testresult-btn-area">
+                    <button className="testresult-btn">다시하기</button>
+                    <button className="testresult-btn">돌아가기</button>
+                </div>
+                <div className="testresult-icon-area">
+                    <div className="testresult-icon-box">
+                        <div className="testresult-icon pointer"><FontAwesomeIcon icon={faInstagram} /></div>
+                        <div className="testresult-icon pointer"><FontAwesomeIcon icon={faFacebookF} /></div>
+                        <div className="testresult-icon pointer"><FontAwesomeIcon icon={faPaperclip} /></div>
+                    </div>
                 </div>
             </div>
         </div>

ac1f68242d675a21ce726d3e34400cadf1f93e97
Eojoonhyuk <94384392+Eojoonhyuk@users.noreply.github.com>
2022-07-19 23:41:40 +0900

    추천받기 수정

--- src/pages/Recommend/index.js
+++ src/pages/Recommend/index.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import "./Recommend.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
+import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 const Recommend = () => {
     return(
@@ -15,12 +17,35 @@ const Recommend = () => {
                     </div>
                 </div>
                 <div className="content-container">
-                    <div className="ranking">
-                        <img className="rankImage" alt="rankImage" src="img/rank1.jpg"/>
+                    <div className="slider-wrap">
+                        <ul className="list-slider" id="slider">
+                            <li>
+                                <img className="rankImage" alt="rankImage" src="img/red.jpeg"/>
+                            </li>
+                            <li>
+                                <img className="rankImage" alt="rankImage" src="img/orange.jpeg"/>
+                            </li>
+                            <li>
+                                <img className="rankImage" alt="rankImage" src="img/yellow.jpeg"/>
+                            </li>
+                            <li>
+                                <img className="rankImage" alt="rankImage" src="img/green.jpeg"/>
+                            </li>
+                            <li>
+                                <img className="rankImage" alt="rankImage" src="img/blue.jpeg"/>
+                            </li>
+                            <li>
+                                <img className="rankImage" alt="rankImage" src="img/indigo.jpeg"/>
+                            </li>
+                            <li>
+                                <img className="rankImage" alt="rankImage" src="img/violet.jpeg"/>
+                            </li>
+                        </ul>
+                    <div className="btn previous" id="previous"><FontAwesomeIcon icon={faAngleLeft} /></div>
+                    <div className="btn next" id="next"><FontAwesomeIcon icon={faAngleRight} /></div>
                     </div>
-                    <div className="filter">
-                        필터
-                    </div>
+
+                    
                     <div className="list">
                         <div className="list-item">
                             <div className="img-area">image</div>

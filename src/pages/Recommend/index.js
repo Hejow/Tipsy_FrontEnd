6f9cca792f6c7125e01413382d70a0995797f9e3
Eojoonhyuk <94384392+Eojoonhyuk@users.noreply.github.com>
2022-07-21 20:44:14 +0900

    추천받기 레이아웃 수정

--- src/pages/Recommend/index.js
+++ src/pages/Recommend/index.js
@@ -1,77 +1,40 @@
 import React from 'react'
 import "./Recommend.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"; 
-import { faAngleRight, faAngleLeft } from '@fortawesome/free-solid-svg-icons';
 
 const Recommend = () => {
     return(
         <div className="recommend-area">
-            <div className="recommend-container">
-                <div className="nav">
-                    <div>
-                        <ul className="nav-list">
-                            <li className="list-row">칵테일</li>
-                            <li className="list-row">위스키</li>
-                            <li className="list-row">와인</li>
-                        </ul>
+            <div className="content">
+                <div className="title"></div>
+                <div className="filter"></div>
+                <div className="item-container">
+                    <div className="item">
+                        <div className="left">와인</div>
+                        <div className="mid">이미지 슬라이드</div>
+                        <div className="right">아이템 리스트
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>    
                     </div>
-                </div>
-                <div className="content-container">
-                    <div className="slider-wrap">
-                        <ul className="list-slider" id="slider">
-                            <li>
-                                <img className="rankImage" alt="rankImage" src="img/red.jpeg"/>
-                            </li>
-                            <li>
-                                <img className="rankImage" alt="rankImage" src="img/orange.jpeg"/>
-                            </li>
-                            <li>
-                                <img className="rankImage" alt="rankImage" src="img/yellow.jpeg"/>
-                            </li>
-                            <li>
-                                <img className="rankImage" alt="rankImage" src="img/green.jpeg"/>
-                            </li>
-                            <li>
-                                <img className="rankImage" alt="rankImage" src="img/blue.jpeg"/>
-                            </li>
-                            <li>
-                                <img className="rankImage" alt="rankImage" src="img/indigo.jpeg"/>
-                            </li>
-                            <li>
-                                <img className="rankImage" alt="rankImage" src="img/violet.jpeg"/>
-                            </li>
-                        </ul>
-                    <div className="btn previous" id="previous"><FontAwesomeIcon icon={faAngleLeft} /></div>
-                    <div className="btn next" id="next"><FontAwesomeIcon icon={faAngleRight} /></div>
+                    <div className="item">
+                        <div className="left">위스키</div>
+                        <div className="mid">이미지 슬라이드</div>
+                        <div className="right">아이템 리스트
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>    
                     </div>
-
-                    
-                    <div className="list">
-                        <div className="list-item">
-                            <div className="img-area">image</div>
-                            <div className="information">information</div>
-                        </div>
-                        <div className="list-item">
-                            <div className="img-area">image</div>
-                            <div className="information">information</div>
-                        </div>
-                        <div className="list-item">
-                            <div className="img-area">image</div>
-                            <div className="information">information</div>
-                        </div>
-                        <div className="list-item">
-                            <div className="img-area">image</div>
-                            <div className="information">information</div>
-                        </div>
-                        <div className="list-item">
-                            <div className="img-area">image</div>
-                            <div className="information">information</div>
-                        </div>
-                        <div className="list-item">
-                            <div className="img-area">image</div>
-                            <div className="information">information</div>
-                        </div>
-
+                    <div className="item">
+                        <div className="left">칵테일</div>
+                        <div className="mid">이미지 슬라이드</div>
+                        <div className="right">아이템 리스트
+                            <div></div>
+                            <div></div>
+                            <div></div>
+                        </div>    
                     </div>
                 </div>
             </div>

39f574df3807b7a81a1b9a579b609c67760e707d
Eojoonhyuk <94384392+Eojoonhyuk@users.noreply.github.com>
2022-08-29 16:59:47 +0900

    추천받기 수정

--- src/pages/Recommend/index.js
+++ src/pages/Recommend/index.js
@@ -18,39 +18,15 @@ const Recommend = () => {
             setSearchParams({q: e.target.value})
         }
     },[setSearchParams])
-    
-    const PopularItem = ({number, image, itemName}) => {
-        return(
-        <a href="/" className="popular-item">
-            <span className="num">{number}</span>
-            <div className="img-box"><img className="img" src={image}></img></div>
-            <p className="item-name">{itemName}</p>
-        </a>
-        );
-    };
 
-    const RecommendItem = ({image, itemName}) => {
-        return (
-        <li className="recommend-item">
-        <a className="left" href="/">
-            <img className="recommend-img" src={image}></img>
-        </a>
-        <div className="right">
-            <div className="explain-box">
-                <p className="recommend-item-name">{itemName}</p>
-                <p className="recommend-item-contry">국가/생산지역: 미국</p>
-                <p className="recommend-item-company">수입사: 신세계엘엔비</p>
-            </div>
-            <ul className="item-tag">
-                <li><a href='/'>#달콤한 맛</a></li>
-                <li><a href='/'>#약한 도수</a></li>
-                <li><a href='/'>#값이 싼</a></li>
-                <li><a href='/'>#가벼운</a></li>
-            </ul>
-        </div>
-    </li>
-        );
-    };
+    const recommendItem = [
+        {id: 1, title: "헌드레드 에이커", img:"img/와인1.png", tags: ["#달콤한 맛", "#약한 도수", "#값이 싼", "#가벼운"]},
+        {id: 2, title: "리카솔리", img:"img/와인2.png", tags: ["태그1", "태그2", "태그3", "태그4"]},
+        {id: 3, title: "볼게리 로쏘", img:"img/와인3.png", tags: ["태그1", "태그2", "태그3", "태그4"]},
+        {id: 4, title: "라포스톨", img:"img/와인4.png", tags: ["태그1", "태그2", "태그3", "태그4"]},
+        {id: 5, title: "샤또팔레 카디날", img:"img/와인5.png", tags: ["태그1", "태그2", "태그3", "태그4"]}
+    ];
+    
     
     return(
         <div className="recommend-area">
@@ -59,16 +35,18 @@ const Recommend = () => {
                     <h2>최근 급상승 인기 상품을 소개해드립니다.</h2>
                     <h1>와인 인기 조회 상품</h1>
                     <div className="popular-items">
-                        <PopularItem number= "01" image="img/와인1.png" itemName="헌드레드에이커"/>
-                        <PopularItem number= "02" image="img/와인2.png" itemName="헌드레드에이커"/>
-                        <PopularItem number= "03" image="img/와인3.png" itemName="헌드레드에이커"/>
-                        <PopularItem number= "04" image="img/와인4.png" itemName="헌드레드에이커"/>
-                        <PopularItem number= "05" image="img/와인5.png" itemName="헌드레드에이커"/>
+                        {recommendItem.map(item =>(
+                            <a key={item.id} href="/" className="popular-item">
+                                <span className="num">{item.id.toString()}</span>
+                                <div className="img-box"><img className="img" src={item.img}></img></div>
+                                <p className="item-name">{item.title}</p>
+                            </a>
+                        ))}
                     </div>
                 </div>
                 <div className="recommend-box">
                     <div className="bar">
-                        <div className="total-num">전체 5,357개</div>
+                        <div className="total-num">전체 {recommendItem.length}개</div>
                         <div className="search-area">
                             <input value={searchText} 
                             type="text" 
@@ -77,16 +55,29 @@ const Recommend = () => {
                             onChange={onChangeInput}
                             onKeyUp={OnKeyUp}></input>   
                         </div>
-                        <div className="filter-area"></div>
+                        {/* <div className="filter-area"></div> */}
                     </div>
                     <div>
                         <ul className="recommend-items">
-                            <RecommendItem image="img/와인1.png" itemName="헌드레드에이커"/>
-                            <RecommendItem image="img/와인2.png" itemName="헌드레드에이커"/>
-                            <RecommendItem image="img/와인3.png" itemName="헌드레드에이커"/>
-                            <RecommendItem image="img/와인4.png" itemName="헌드레드에이커"/>
-                            <RecommendItem image="img/와인5.png" itemName="헌드레드에이커"/>
-                            <RecommendItem image="img/와인2.png" itemName="헌드레드에이커"/>
+                            {recommendItem.map(item => (
+                                <li className="recommend-item">
+                                    <a className="left" href="/">
+                                        <img className="recommend-img" src={item.img}></img>
+                                    </a>
+                                    <div className="right">
+                                        <div className="explain-box">
+                                            <p className="recommend-item-name">{item.title}</p>
+                                            <p className="recommend-item-contry">국가/생산지역: 미국</p>
+                                            <p className="recommend-item-company">수입사: 신세계엘엔비</p>
+                                        </div>
+                                        <ul className="item-tag">
+                                            {item.tags.map(tag => (
+                                                <li><a href='/'>{tag}</a></li>
+                                            ))}
+                                        </ul>
+                                    </div>
+                                </li>
+                            ))}
                         </ul>
                     </div>
                 </div>

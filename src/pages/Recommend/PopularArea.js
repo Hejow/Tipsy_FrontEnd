c12017f2c7d3e814cae6e10a67599f89e2bfb4bb
Moon He Jow <gmlwh124@naver.com>
2022-10-23 18:09:43 +0900

    feat: Alter Get Func, Refactor Codes

--- src/pages/Recommend/PopularArea.js
+++ src/pages/Recommend/PopularArea.js
@@ -1,10 +1,31 @@
 import React from "react";
-import Slider from "./ImageSlider";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 import SearchInput from './SearchInput';
+
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
 
-const PopularArea = ({recommendItem}) => {
+const PopularArea = ({recommends}) => {
+    const setting = {
+        infinity: true,
+        speed: 50,
+        slidesToshow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoPlayspeed: 200,
+        variableWidth: true,
+        arrow: false,
+    };
+
+    let rank = 1;
+    const topRecommends = recommends.sort((a,b) => a["clicked"] - b["clicked"]).slice(0, 10).map(item => ({
+        rank: rank++,
+        ...item,
+    }));
+
     return(
         <div className="popular-box">
             <div className="search-area">
@@ -12,7 +33,19 @@ const PopularArea = ({recommendItem}) => {
                 <SearchInput/>
             </div>
             <h2>Top 10</h2>
-            <Slider item={recommendItem}/>            
+            <Slider className="popular-items" {...setting}>
+                {topRecommends.map(item => (
+                    <div key={item.name} className="popular-item">
+                        <div className="popularItemHead">
+                            <span className="num">{item.rank}</span>
+                            <p className="item-name">{item.name}</p>
+                        </div>
+                        <div className="img-box">
+                            <img className="img" alt="와인이미지" src={item.img}/>
+                        </div>
+                    </div>
+                ))}
+            </Slider>
         </div>
     );
 }
--- src/pages/Recommend/RePagination.js
+++ src/pages/Recommend/RePagination.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect } from 'react';
 import ReactPaginate from 'react-paginate';
 
-const RePagination = ({recommendItem, setCurentItems}) => {
+const RePagination = ({recommends, setCurentItems}) => {
     const itemsPerPage = 6; // 한 페이지당 몇 개의 아이템을 보여줄지
     const [pageCount, setPageCount] = useState(0) // 검색결과에 따라 다름
     const [itemOffset, setItemOffset] = useState(0) // 데이터를 가져왔는데 어디서부터 어디까지 자를건지
@@ -9,12 +9,12 @@ const RePagination = ({recommendItem, setCurentItems}) => {
     useEffect(()=>{
       const endOffset = itemOffset + itemsPerPage;
       // console.log(`Loading items from ${itemOffset} to ${endOffset}`);
-      setCurentItems(recommendItem.slice(itemOffset, endOffset))
-      setPageCount(Math.ceil((recommendItem.length ?? 0 / itemsPerPage)))
-    },[itemOffset, itemsPerPage, setCurentItems, recommendItem])
+      setCurentItems(recommends.slice(itemOffset, endOffset))
+      setPageCount(Math.ceil((recommends.length ?? 0 / itemsPerPage)))
+    },[itemOffset, itemsPerPage, setCurentItems, recommends])
   
     const handlePageClick = (e)=>{
-      setItemOffset(e.selected * itemsPerPage % recommendItem.length)
+      setItemOffset(e.selected * itemsPerPage % recommends.length)
     }
   
     return(

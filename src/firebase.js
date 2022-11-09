6737bfde8c378744399d531df9509e0d48987d2c
Moon He Jow <gmlwh124@naver.com>
2022-11-09 09:08:36 +0900

    hotFix: insert Firebase Key

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyDvPRlT6OV5eA0HvcoY45u9DAXhUYPem1A",
+  authDomain: "mytype-8123d.firebaseapp.com",
+  projectId: "mytype-8123d",
+  storageBucket: "mytype-8123d.appspot.com",
+  messagingSenderId: "991764435907",
+  appId: "1:991764435907:web:faeb138b8e905a29906376",
+  measurementId: "G-ETDJ82HYZZ"
 };
 
 const app = initializeApp(firebaseConfig);
